2f6efde2bc7b27e7699a306da21887e9690ae576
Dongeun Paeng <dongeun.paeng@gmail.com>
2020-09-09 02:57:56 +0900

    Changed folder structure

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => (
+  <div>
+    <div className="widget-header">
+      <h3 className="widget-header__title">Login</h3>
+    </div>
+    Login
+  </div>
+);
+
+export default Login;
---
+++ src/pages/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Signup = props => (
+  <div>
+    <div className="widget-header">
+      <h3 className="widget-header__title">Signup</h3>
+    </div>
+    Signup
+  </div>
+);
+
+export default Signup;
